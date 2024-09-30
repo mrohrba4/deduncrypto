31cbd747b6fa1babd5927db780ca46a58b4e9a5c
Michael Rohrbaugh <74413637+mrohrba4@users.noreply.github.com>
2024-09-30 15:33:05 -0400

    Updated landing page with logo, and improved button styles

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,40 +1,78 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types';
 import { RootStackParamList } from '../types';
-
-
+import logo from '../../assets/images/DedunCryptoLogo.png';
+import * as Font from 'expo-font';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Landing'>;
 
 const LandingScreen: React.FC<Props> = ({ navigation }) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const loadFonts = async () => {
+            await Font.loadAsync({
+                'Exo 2': require('../../assets/fonts/Exo2-VariableFont_wght.ttf'),
+            });
+            setIsLoaded(true);
+        };
+
+        loadFonts();
+    }, []);
+
+    if (!isLoaded) {
+        return null;
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Dedun Crypto</Text>
-            <Button
-                title="Login"
-                onPress={() => navigation.navigate('Login')}
+            <Image
+                source={logo}
+                style={styles.logo}
+                resizeMode="contain"
             />
-            <Button
-                title="Sign Up"
-                onPress={() => navigation.navigate('Signup')}
-            />
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Login')} // Navigate to Login
+            >
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Signup')} // Navigate to Sign Up
+            >
+                <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#000',  // Dark background
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000',
     },
-    title: {
-      fontSize: 32,
-      color: '#00ADEF',  // Futuristic blue
-      marginBottom: 40,
+    logo: {
+        width: 350,
+        height: 350,
+        marginBottom: 20
     },
-  });
+    button: {
+        backgroundColor: 'rgba(30, 144, 255, 0.2)', 
+        width: '80%', 
+        paddingVertical: 15, 
+        borderRadius: 30, 
+        alignItems: 'center', 
+        marginVertical: 10, 
+    },
+    buttonText: {
+        color: 'white', 
+        fontSize: 18, 
+        fontWeight: 'bold', 
+    },
+});
 
-  export default LandingScreen;
+export default LandingScreen;
