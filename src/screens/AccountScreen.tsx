ff5c6e7e9b32795a5ce8f7633d74476db22b9a14
Michael Rohrbaugh <74413637+mrohrba4@users.noreply.github.com>
2024-10-01 14:34:19 -0400

    Match design of login, and account screens with landing screen design

--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import { Alert, View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { deleteAccount, changePassword } from '../services/authService';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
@@ -55,9 +55,22 @@ const AccountScreen: React.FC = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Account Settings</Text>
-            <Button title="Logout" onPress={handleLogout} />
-            <Button title="Delete Account" onPress={handleDeleteAccount} color="red" />
+
+            {/* Logout Button */}
+            <Text style={styles.label}>Logout</Text>
+            <TouchableOpacity style={styles.button} onPress={handleLogout}>
+                <Text style={styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
             
+
+            {/* Delete Account Button */}
+            <Text style={styles.label}>Delete Account</Text>
+            <TouchableOpacity style={styles.button} onPress={handleDeleteAccount}>
+                <Text style={styles.buttonText}>Delete Account</Text>
+            </TouchableOpacity>
+            
+            {/* Change Password Button */}
+            <Text style={styles.label}>Change Password</Text>
             <TextInput
                 placeholder="Enter new password"
                 value={newPassword}  // Link state to input
@@ -65,15 +78,56 @@ const AccountScreen: React.FC = () => {
                 secureTextEntry
                 style={styles.input}
             />
-            <Button title="Change Password" onPress={handleChangePassword} />
+            <TouchableOpacity style={styles.button} onPress={handleChangePassword}>
+                <Text style={styles.buttonText}>Change Password</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', padding: 20 },
-    title: { fontSize: 24, marginBottom: 20 },
-    input: { marginBottom: 15, borderBottomWidth: 1, borderColor: '#ccc', padding: 10 },
+    container: { 
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center', 
+        padding: 20,
+        backgroundColor: '#000',
+    },
+    title: { 
+        fontSize: 24, 
+        marginBottom: 20,
+        color: '#ffffff',
+        textDecorationLine: 'underline',  
+    },
+    label: { 
+        fontSize: 16, 
+        marginBottom: 5,
+        color: '#ffffff',
+        textDecorationLine: 'underline', 
+    },
+    input: { 
+        marginBottom: 15,
+        width: '80%', 
+        borderBottomWidth: 1, 
+        borderColor: '#ccc', 
+        padding: 10,
+        backgroundColor: '#1e1e1e',
+        color: '#FFFFFF',
+        textAlign: 'center', 
+    },
+    button: {
+        backgroundColor: 'rgba(30, 144, 255, 0.2)', 
+        width: '80%', 
+        paddingVertical: 15, 
+        borderRadius: 30, 
+        alignItems: 'center', 
+        marginVertical: 10,
+        marginBottom: 30, 
+    },
+    buttonText: {
+        color: 'white', 
+        fontSize: 13, 
+    },
 });
 
 export default AccountScreen;
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { login } from '../services/authService';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../types';
+import logo from '../../assets/images/DedunCryptoLogo.png';
 
 
 // Define custom error:
@@ -37,10 +38,12 @@ interface FirebaseError extends Error {
 
     return (
         <View style={styles.container}>
+            <Image source={logo} style={styles.logo} />
             <Text style={styles.title}>Login</Text>
             {error && <Text style={styles.error}>{error}</Text>}
             <TextInput
                 placeholder="Email"
+                placeholderTextColor="#ffffff"
                 value={email}
                 onChangeText={setEmail}
                 style={styles.input}
@@ -51,18 +54,63 @@ interface FirebaseError extends Error {
                 onChangeText={setPassword}
                 secureTextEntry
                 style={styles.input}
+                placeholderTextColor="#ffffff"
             />
-            <Button title="Login" onPress={handleLogin}/>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleLogin} // Navigate to Login
+            >
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
         </View>
     )
 
   }
 
   const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', padding: 20 },
-    title: { fontSize: 24, marginBottom: 20 },
-    input: { marginBottom: 15, borderBottomWidth: 1, borderColor: '#ccc', padding: 10 },
-    error: { color: 'red', marginBottom: 15 },
+    container: { 
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',  
+        padding: 20,
+        backgroundColor: '#000', 
+    },
+    title: { 
+        fontSize: 24, 
+        marginBottom: 20 
+    },
+    logo: {
+        width: 350,
+        height: 350,
+        marginBottom: 20
+    },
+    input: { 
+        marginBottom: 15,
+        width: '80%', 
+        borderBottomWidth: 1, 
+        borderColor: '#ccc', 
+        padding: 10,
+        backgroundColor: '#1e1e1e',
+        color: '#FFFFFF',
+        textAlign: 'center',
+    },
+    button: {
+        backgroundColor: 'rgba(30, 144, 255, 0.2)', 
+        width: '80%', 
+        paddingVertical: 15, 
+        borderRadius: 30, 
+        alignItems: 'center', 
+        marginVertical: 10, 
+    },
+    buttonText: {
+        color: 'white', 
+        fontSize: 18, 
+        fontWeight: 'bold', 
+    },
+    error: { 
+        color: 'red', 
+        marginBottom: 15 
+    },
 });
 
   export default LoginScreen;
